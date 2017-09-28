bc4f0d4c39ed3b373cf6dbe9d2a3567441ee754f
MotsoVitalii <vitalii.motso@gmail.com>
2017-09-28 14:14:56 +0300

    add configurator values test

---
+++ openprocurement/tender/belowthreshold/tests/configurator.py
@@ -0,0 +1,19 @@
+import unittest
+from openprocurement.tender.belowthreshold.adapters import TenderBelowThersholdConfigurator
+
+
+class ConfiguratorValueTest(unittest.TestCase):
+
+    def test_reverse_awarding_criteria(self):
+        if hasattr(TenderBelowThersholdConfigurator, 'reverse_awarding_criteria'):
+            self.assertEqual(TenderBelowThersholdConfigurator.reverse_awarding_criteria, False)
+
+
+def suite():
+    current_suite = unittest.TestSuite()
+    current_suite.addTest(unittest.makeSuite(ConfiguratorValueTest))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
