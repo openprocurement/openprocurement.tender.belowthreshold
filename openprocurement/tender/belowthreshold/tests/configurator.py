93131a1b30ad64e7619155b3651894c976b0a9bd
MotsoVitalii <vitalii.motso@gmail.com>
2017-10-02 17:22:05 +0300

    less commits

--- openprocurement/tender/belowthreshold/tests/configurator.py
+++ openprocurement/tender/belowthreshold/tests/configurator.py
@@ -1,17 +1,19 @@
 import unittest
+from openprocurement.tender.core.tests.configurator import ConfiguratorTestMixin
 from openprocurement.tender.belowthreshold.adapters import TenderBelowThersholdConfigurator
+from openprocurement.tender.belowthreshold.models import Tender
 
 
-class ConfiguratorValueTest(unittest.TestCase):
-
-    def test_reverse_awarding_criteria(self):
-        if hasattr(TenderBelowThersholdConfigurator, 'reverse_awarding_criteria'):
-            self.assertEqual(TenderBelowThersholdConfigurator.reverse_awarding_criteria, False)
+class ConfiguratorTest(unittest.TestCase, ConfiguratorTestMixin):
+    configurator_class = TenderBelowThersholdConfigurator
+    reverse_awarding_criteria = False
+    awarding_criteria_key = 'not yet implemented'
+    configurator_model = Tender
 
 
 def suite():
     current_suite = unittest.TestSuite()
-    current_suite.addTest(unittest.makeSuite(ConfiguratorValueTest))
+    current_suite.addTest(unittest.makeSuite(ConfiguratorTest))
     return suite
 
 
